7cb60067c32663aae8a4943d305f360788199c66
Aditya Aggarwal <aditya@cybersapien.xyz>
2016-12-26 23:54:07 +0530

    Updated the codes to the new convention
    Updated MergeSort.java and QuickSort.java to match the Code Convention changes.

--- Sorting Algorithms/Quick Sort Algorithm/Java/QuickSort.java
+++ Sorting Algorithms/Quick Sort Algorithm/Java/QuickSort.java
@@ -14,24 +14,24 @@ import java.util.Scanner;
 public class QuickSort{
 
 	public static void main(String[] args) {
-        //Initialize the Scanner Object to take input
+        // Initialize the Scanner Object to take input
         Scanner in = new Scanner(System.in);
-        //Get the Size of the Array
+        // Get the Size of the Array
         System.out.println("Enter the size of the array:");
         int size = in.nextInt();
-        //Initialize the Array for sorting the elements
+        // Initialize the Array for sorting the elements
         int[] array = new int[size];
-        //Get the data from user
+        // Get the data from user
         System.out.println("Enter the elements of the array: ");
 
         for (int i = 0; i < size; i++) {
             array[i] = in.nextInt();
         }
 
-        //Call the quickSort function to sort the Array
+        // Call the quickSort function to sort the Array
         quickSort(array, 0, size - 1);
 
-        //Print the Sorted Array
+        // Print the Sorted Array
         System.out.println("Sorted Array: ");
 
         for (int a : array) {
@@ -42,20 +42,20 @@ public class QuickSort{
     }
 
     public static void quickSort(int[] arr, int low, int high) {
-        //Check if low is smaller than high
-        //This is the exit condition for the recursive solution
+        // Check if low is smaller than high
+        // This is the exit condition for the recursive solution
         if (low < high){
-        	//partition and swap the array
+        	// Partition and swap the array
             int partitionIndex = partition(arr, low, high);
-            //Quicksort the left of the partition
+            // Quicksort the left of the partition
             quickSort(arr, low, partitionIndex - 1);
-            //Quicksort the right of the partition
+            // Quicksort the right of the partition
             quickSort(arr, partitionIndex + 1, high);
         }
     }
 
     private static int partition(int arr[], int low, int high){
-        //Select the pivot as the highest element in the array
+        // Select the pivot as the highest element in the array
         int pivot = arr[high];
         int i = low - 1;
 
