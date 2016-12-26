85c27695d94bff88f9d5e114264296b12aa8575b
Aditya Aggarwal <aditya@cybersapien.xyz>
2016-12-26 22:10:25 +0530

    Updated with newer Coding Convention

--- Sorting Algorithms/Quick Sort Algorithm/Java/QuickSortDijsktra.java
+++ Sorting Algorithms/Quick Sort Algorithm/Java/QuickSortDijsktra.java
@@ -14,24 +14,24 @@ import java.util.Scanner;
 public class QuickSortDijsktra{
 
 	public static void main(String[] args) {
-        //Initialize the Scanner Object to take input
+        // Initialize the Scanner Object to take input
         Scanner in = new Scanner(System.in);
-        //Get the Size of the Array
+        // Get the Size of the Array
         System.out.println("Enter the size of the array:");
         int size = in.nextInt();
-        //Initialize the Array for sorting the elements
+        // Initialize the Array for sorting the elements
         int[] array = new int[size];
-        //Get the data from user
+        // Get the data from user
         System.out.println("Enter the elements of the array: ");
 
         for (int i = 0; i < size; i++) {
             array[i] = in.nextInt();
         }
 
-        //Call the quickSort function to sort the Array
+        // Call the quickSort function to sort the Array
         quickSortDijsktra(array, 0, size - 1);
 
-        //Print the Sorted Array
+        // Print the Sorted Array
         System.out.println("Sorted Array: ");
 
         for (int a : array) {
@@ -42,9 +42,8 @@ public class QuickSortDijsktra{
     }
 
     public static void quickSortDijsktra(int[] array, int low, int high){
-    	//Break condition for the Recursive Code
+    	// Break condition for the Recursive Code
     	if (low >= high) {
-    		
     		return;
     	}
 
@@ -52,11 +51,18 @@ public class QuickSortDijsktra{
     	int ht = high;
     	int pivot = array[low];
 
+    	// The loop here divides the Array in three different part
     	for (int i = low + 1; i <= high; i++) {
+
+    		// This is to make sure that we don't get array out of bounds exceptions
     		if (lt == ht) {
     			break;
     		}
 
+    		// Swap the left hand element with the i-th element if the i-th element
+    		// is lesser than the pivot
+    		// else swap the right hand element with the i-th element if the i-th element
+    		// is greater than pivot
     		if (array[i] < pivot) {
     			int swap = array[lt];
     			array[lt] = array[i];
