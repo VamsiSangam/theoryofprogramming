907f73ccb2f120556ed336f28ea328c2757c2fe5
Saurabh Rane <ranesr@users.noreply.github.com>
2017-03-10 09:11:37 -0600

    Updating JumpSearch.py
    Using math library to find out integer square root

--- Searching Algorithms/Jump Search Algorithm/Python/JumpSearch.py
+++ Searching Algorithms/Jump Search Algorithm/Python/JumpSearch.py
@@ -9,14 +9,15 @@
 #
 # ===== ===== ===== */
 
+import math
 
-# Jump Search Code
+# Jump Search Function
 def jump_search(alist, val):
 	
 	length = len(alist)
 	
 	# Calculating jump
-	jump = _integer_sqrt(length)
+	jump = int(math.sqrt(length))
 	
 	left, right = 0, 0
 
@@ -40,43 +41,12 @@ def jump_search(alist, val):
 		i += 1
 	
 	return -1
-
-
-# Function for finding integer square root
-# Details are here: http://en.wikipedia.org/wiki/Integer_square_root
-def _integer_sqrt(n):
-
-	if n < 0:
-		raise ValueError('Square root not defined for negative numbers...')
-		
-	# Find greatest shift.
-	shift = 2
-	nShifted = n >> shift
-	
-	# We check for nShifted being n, since some implementations of logical right shifting shift modulo the word size.
-	while nShifted != 0 and nShifted != n:
-		shift += 2
-		nShifted = n >> shift
-	
-	shift -= 2
-	
-	# Find digits of result.
-	result = 0
-	while shift >= 0:
-		result = result << 1
-		candidateResult = result + 1
-		
-		if (candidateResult * candidateResult) <= (n >> shift):
-			result = candidateResult
-		shift -= 2
-	
-	return result
 	
     
 # Examples
 alist = [1, 3, 5, 7, 9, 11, 13, 15, 17]
 # Jump Search Results
-print(jump_search(alist, 0))	#-1
-print(jump_search(alist, 3))	# 1
-print(jump_search(alist, 11))	# 5
+print(jump_search(alist, 0))	# -1
+print(jump_search(alist, 3))	#  1
+print(jump_search(alist, 11))	#  5
 print(jump_search(alist, 16))	# -1
