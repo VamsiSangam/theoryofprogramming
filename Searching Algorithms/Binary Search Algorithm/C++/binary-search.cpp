68388b0040936f6e305a637ca7212959bd267053
praveenmylavarapu <saipraveenmylavarapu@gmail.com>
2017-07-01 10:59:37 -0400

    Made suggested changes

--- Searching Algorithms/Binary Search Algorithm/C++/binary-search.cpp
+++ Searching Algorithms/Binary Search Algorithm/C++/binary-search.cpp
@@ -5,7 +5,8 @@ Theory of Programming
 Binary Search Algorithm
 http://theoryofprogramming.com/2016/11/04/binary-search-algorithm/
 GitHub - https://github.com/VamsiSangam/theoryofprogramming
-Code Contributor - Praveen Mylavarapu (github.com/praveenmylavarapu, saipraveen.ml)
+Code Contributors - <salman-bhai>
+                  - Praveen Mylavarapu (github.com/praveenmylavarapu, saipraveen.ml)
 
 ===== ===== ===== */
 
@@ -14,59 +15,62 @@ Code Contributor - Praveen Mylavarapu (github.com/praveenmylavarapu, saipraveen.
 using namespace std;
 
 // Main Binary Search logic
-int binarySearch(vector<int> v,int key) {
+int binarySearch(vector<int> v, int val) {
 	int first = 0;
 	int last = v.size() - 1;
 
 	while(first <= last) {
 		int mid = ((first + last) / 2);
-		// If the middle element is the key, return the middle element
-		if(v[mid] == key) {
+		// If the middle element is the value, return the middle element
+		if(v[mid] == val) {
 			return mid;
 		}
-		// If the key is lesser than the middle element, then then it is in the left most region
-		else if(v[mid] > key) {
+		// If the value is lesser than the middle element, then then it is in the left most region
+		else if(v[mid] > val) {
 			last = mid - 1;
 		}
-		// If the key is greater than the middle element, then it is in the right most region
+		// If the value is greater than the middle element, then it is in the right most region
 		else {
 			first = mid + 1;
 		}
 	}
 
-	// If key isn't found, return -1
+	// If value isn't found, return -1
 	return -1;
 }
 
 int main() {
-
     // Variable declaration
     vector<int> v;
 
 	// Enter number of elements in the vector
 	int n, temp;
-	cout<<"Enter the number of elements: ";
+	cout << "Enter the number of elements: ";
 	cin >> n;
 
 	// Enter the elements in the vector
+	cout << "Enter " << n << " elements: ";
+	
 	for(int i = 0; i < n ; i ++) {
 		cin >> temp;
 		v.push_back(temp);
 	}
 
 	// Enter the element to be searched
-	int key;
-	cin >> key;
+	int val;
+	cout << "Enter the element to be searched: ";
+	cin >> val;
 
-	int x = binarySearch(v,key);
+	int x = binarySearch(v, val);
 
-	// In case -1 is found, means key isn't present
+	// In case -1 is found, means value isn't present
 	if(x == -1) {
-		cout<<"Key not found!"<<endl;
+		cout << val << " not found!" << endl;
 	}
-	// If -1 isn't returned, key is found!
+	// If -1 isn't returned, value is found!
 	else {
-		cout<<"Key found at position "<< x + 1 <<endl;
+		cout << val << " found at position " << x + 1 << endl;
 	}
+
 	return 0;
-} 
+}
