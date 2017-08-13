3019cbca58377e6982d2d8681d871ba4281d25e8
praveenmylavarapu <saipraveenmylavarapu@gmail.com>
2017-08-13 14:26:08 -0400

    Made requested changes

--- Searching Algorithms/Jump Search Algorithm/C++/JumpSearch.cpp
+++ Searching Algorithms/Jump Search Algorithm/C++/JumpSearch.cpp
@@ -14,40 +14,39 @@ Code Contributor - Praveen Mylavarapu (saipraveen.ml)
 
 using namespace std;
 
-bool jumpSearch(int a[100], int n, int value)
+int jumpSearch(int arr[], int n, int value)
 {
-    int i, right = n, jump = (int) sqrt(n);
+    int i, right, left, jump = (int) sqrt(n);
 
-    for (i = 0; i + jump < n && a[i] < value; i += jump);
+    // Keep jumping until value is between any two elements of the array or when i goes out of bound
+    for (i = 0; i + jump < n && arr[i] < value; i += jump);
 
-    if (a[i] >= value)
+    if (value <= arr[i])    // right is i if search value is less than any jumped element in the array. If not, right is last element
         right = i;
+    else
+        right = n;
 
-    while (right >= i - jump)
-        if (a[right--] == value)
-            return 0;
+    left = right - jump;
 
-    return 1;
+    if (left < 0)   // ensuring left dosen't go negaitive
+        left = 0;
+
+    // Linear search backwards
+    while (right >= left)
+        if (arr[right--] == value)
+            return right + 1;
+
+    return -1;
 }
 
 int main()
 {
-    int a[100], i = 0, n, value;
+    int arr[] = {1, 3, 5, 7, 9, 11, 13, 15}, n = sizeof(arr)/sizeof(int);
 
-    cout << "Enter number of elements: ";
-    cin >> n;
-
-    cout << "Enter the elements of search space: ";
-
-    while (i < n)
-        cin >> a[i++];
-
-    cout << "Enter the value to be searched: ";
-    cin >> value;
-
-    if (jumpSearch(a, n, value))
-        cout << "Not ";
-    cout << "Found!" << endl;
+    cout << jumpSearch(arr, n, 0) << endl; // -1
+    cout << jumpSearch(arr, n, 3) << endl; // 1
+    cout << jumpSearch(arr, n, 11) << endl; // 5
+    cout << jumpSearch(arr, n, 16) << endl; // -1
 
     return 0;
 }
